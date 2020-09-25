0c30f11dc1fca6198059c39546a43a3911bf9404
Megan Kuo <61397197+megankuo@users.noreply.github.com>
2020-09-24 17:34:25 -0700

    Update lab-three.js
    Debugged getDayOfTheWeek()

--- lab-three.js
+++ lab-three.js
@@ -43,7 +43,7 @@ function getMonthCode(month, year) {
             code = 6;
             break;
         default:
-            console.log('Month code error')
+            console.log('Month code error');
     }
     if ( (year >= 1600 && year < 1700) || (year >= 2000 && year < 2100) ) {
         code += 6;
@@ -59,26 +59,17 @@ function getMonthCode(month, year) {
 function getDayOfTheWeek(year, month, day) {
     // Identify last 2 digits of input year
     const yy = year.toString().substr(-2);
-    console.log(yy);
     // Identify month code with consideration of leap year
     let monthCode = getMonthCode(month, year);
-    if (isLeapYear(year)) {
+    if (isLeapYear(year) && (month === 'Jan' || month === 'Feb')) {
         monthCode -= 1;
     }
-    console.log(monthCode);
-    //const weekdayCode = Math.floor(((yy / 12 / 4) + day + monthCode) % 7);
-    let weekdayCode1 = Math.floor(yy/12); // 20 fits 1 time
-    let weekdayCode2 = yy - weekdayCode1 * 12; // 20 - 1 * 12 = 8
-    let weekdayCode3 = Math.floor(weekdayCode2/4); // 8/4 = 2
-    let weekdayCode4 = weekdayCode1 + weekdayCode2 + weekdayCode3 + day + monthCode; // 1 + 8 + 2 + 24 + 11 = 46
-    weekdayCode = weekdayCode4 % 7; // 4 <- INCORRECT should be 5
-    console.log(weekdayCode1);
-    console.log(weekdayCode2);
-    console.log(weekdayCode3);
-    console.log(day);
-    console.log(monthCode);
-    console.log(weekdayCode4);
-    console.log(weekdayCode);
+    // Algorithm to determine what day of the week a given date is
+    let weekdayCode1 = Math.floor(yy/12);
+    let weekdayCode2 = yy - weekdayCode1 * 12;
+    let weekdayCode3 = Math.floor(weekdayCode2/4);
+    let weekdayCode4 = weekdayCode1 + weekdayCode2 + weekdayCode3 + day + monthCode;
+    weekdayCode = weekdayCode4 % 7;
     let weekday;
     switch (weekdayCode) {
         case 0:
@@ -107,4 +98,3 @@ function getDayOfTheWeek(year, month, day) {
     }
     return weekday;
 }
-console.log(getDayOfTheWeek(2020, 'Sep', 24)); // printing Wed when it should be Thu
